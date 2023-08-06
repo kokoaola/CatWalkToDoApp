f270050568e2497d9a17255acbb5ce9058fae99c
koala panda <koakoa8823@protonmail.com>
2023-08-06 18:24:16 +0900

    KeisanViewにキーボード閉じる機能を実装

---
+++ ShoppingApp/Model/AppSetting.swift
@@ -0,0 +1,21 @@
+//
+//  AppSetting.swift
+//  ShoppingApp
+//
+//  Created by koala panda on 2023/08/06.
+//
+
+import SwiftUI
+
+class AppSetting{
+    
+    static func colseKeyBoard(){
+        let keyWindow = UIApplication.shared.connectedScenes
+            .filter({$0.activationState == .foregroundActive})
+            .map({$0 as? UIWindowScene})
+            .compactMap({$0})
+            .first?.windows
+            .filter({$0.isKeyWindow}).first
+        keyWindow?.endEditing(true)
+    }
+}
