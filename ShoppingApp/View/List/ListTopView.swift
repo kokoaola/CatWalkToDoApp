bee130a79d80085a74a1d5486f5871034c740061
koala panda <koakoa8823@protonmail.com>
2023-08-09 12:50:02 +0900

    新規追加ボタンをナビゲーションバーのボタンに変更した

--- ShoppingApp/View/List/ListTopView.swift
+++ ShoppingApp/View/List/ListTopView.swift
@@ -33,86 +33,102 @@ struct List_mainView: View {
     
     
     var body: some View {
-        ZStack{
-            VStack {
-                //左上のプラスの追加ボタン
-                HStack{
+        NavigationView{
+            //下部の完了ボタンを配置するためのZStack
+            ZStack{
+                VStack {
+                    
+                    //上に表示される３つのラベル
+                    HStack{
+                        ForEach(0 ..< 3) {num in
+                            VStack{
+                                //表示中だけラベルの色を変える
+                                Rectangle()
+                                    .foregroundColor(.clear)
+                                    .frame(width: UIScreen.main.bounds.width / 3.5, height: 30)
+                                    .overlay(Text("\(labelArray[num])")
+                                        .foregroundColor(Color(selection == num ? UIColor.label : .gray)))
+                                    .opacity(selection == num ? 1.0 : 0.4)
+                                    .onTapGesture {
+                                        selection = num
+                                    }
+                                Color.primary.frame(width: UIScreen.main.bounds.width / 5, height: 2)
+                                    .opacity(selection == num ? 1.0 : 0.0)
+                                    .padding(.top, -5)
+                            }
+                        }
+                    }
+                    
+                    
+                    //買い物リストの中身は洗濯中のタブによって切り替える
+                    TabView(selection: $selection) {
+                        ShoppingList1(filterdList: $itemVM.filterdList0)
+                            .tag(0)
+                        ShoppingList1(filterdList: $itemVM.filterdList1)
+                            .tag(1)
+                        ShoppingList1(filterdList: $itemVM.filterdList2)
+                            .tag(2)
+                    }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                        .environmentObject(itemVM)
+                    
+                }
+                
+                VStack{
                     Spacer()
+                    //買い物完了ボタン
+                    Button(action: {
+                        showCompleteTaskAlert.toggle()
+                    }, label: {
+                        Buttons()
+                        //ボタン本体のデザインは別のファイル
+                    })
+                    .padding(.bottom, 10)
+                }
+                
+                //タスク新規追加用のシート
+                .sheet(isPresented: $showAddNewItemSheet, content: {
+                    AddNewItem(newLabelNum: selection)
+                        .environmentObject(itemVM)
+                })
+                
+                //買い物完了ボタンが押された後の確認アラート
+                .alert(isPresented: $showCompleteTaskAlert){
+                    Alert(title: Text("買い物完了"),
+                          message: Text("チェックした項目を削除して\n買い物を完了にしますか？"),
+                          //OKならチェックした項目をリストから削除（未搭載）
+                          primaryButton: .default(Text("OK"), action: {
+                        itemVM.completeTask()
+                    }),
+                          secondaryButton: .cancel(Text("Cansel"), action:{}))
+                }
+                
+                //ビュー生成時にラベルを配列に追加する
+                .onAppear(){
+                    labelArray = [label0, label1, label2]
+                }
+            }
+            
+            //キーボード閉じるボタン
+            .toolbar {
+                
+                //シート閉じるボタン
+                ToolbarItemGroup(placement: .navigationBarLeading) {
+                    Spacer()
+                    Button("Edit") {
+                        EditButton()
+                    }
+                }
+                
+                //左上のプラスの追加ボタン
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Spacer()
+                    
                     Button(action: {
                         showAddNewItemSheet = true
                     }) {
                         Image(systemName: "plus")
-                            .padding()
                     }
                 }
-                
-                //上に表示される３つのラベル
-                HStack{
-                    ForEach(0 ..< 3) {num in
-                        VStack{
-                            //表示中だけラベルの色を変える
-                            Rectangle()
-                                .foregroundColor(.clear)
-                                .frame(width: UIScreen.main.bounds.width / 3.5, height: 30)
-                                .overlay(Text("\(labelArray[num])")
-                                    .foregroundColor(Color(selection == num ? UIColor.label : .gray)))
-                                .opacity(selection == num ? 1.0 : 0.4)
-                                .onTapGesture {
-                                    selection = num
-                                }
-                            Color.primary.frame(width: UIScreen.main.bounds.width / 5, height: 2)
-                                .opacity(selection == num ? 1.0 : 0.0)
-                                .padding(.top, -5)
-                        }
-                    }
-                }
-                
-                
-                //買い物リストの中身は洗濯中のタブによって切り替える
-                TabView(selection: $selection) {
-                    ShoppingList1(filterdList: $itemVM.filterdList0)
-                        .tag(0)
-                    ShoppingList1(filterdList: $itemVM.filterdList1)
-                        .tag(1)
-                    ShoppingList1(filterdList: $itemVM.filterdList2)
-                        .tag(2)
-                }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-                    .environmentObject(itemVM)
-                
-            }
-            
-            VStack{
-                Spacer()
-                //買い物完了ボタン
-                Button(action: {
-                    showCompleteTaskAlert.toggle()
-                }, label: {
-                    Buttons()
-                    //ボタン本体のデザインは別のファイル
-                })
-                .padding(.bottom, 10)
-            }
-            
-            //タスク新規追加用のシート
-            .sheet(isPresented: $showAddNewItemSheet, content: {
-                AddNewItem(newLabelNum: selection)
-                    .environmentObject(itemVM)
-            })
-            
-            //買い物完了ボタンが押された後の確認アラート
-            .alert(isPresented: $showCompleteTaskAlert){
-                Alert(title: Text("買い物完了"),
-                      message: Text("チェックした項目を削除して\n買い物を完了にしますか？"),
-                      //OKならチェックした項目をリストから削除（未搭載）
-                      primaryButton: .default(Text("OK"), action: {
-                    itemVM.completeTask()
-                }),
-                      secondaryButton: .cancel(Text("Cansel"), action:{}))
-            }
-            
-            //ビュー生成時にラベルを配列に追加する
-            .onAppear(){
-                labelArray = [label0, label1, label2]
             }
         }
     }
