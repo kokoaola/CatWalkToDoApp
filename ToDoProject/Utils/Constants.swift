6dbb6e258f32d790dbcf74af0bf2269156788d69
koala panda <koakoa8823@protonmail.com>
2023-12-19 14:00:43 +0900

    Contantsファイルを作成

---
+++ ToDoProject/Utils/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  ToDoProject
+//
+//  Created by koala panda on 2023/12/19.
+//
+
+import Foundation
+
+
+struct Constants {
+    static let collectionName = "users"
+}
---
+++ ToDoProject/ViewModels/ViewModelBase.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModelBase.swift
+//  ToDoProject
+//
+//  Created by koala panda on 2023/12/19.
+//
+
+import Foundation
