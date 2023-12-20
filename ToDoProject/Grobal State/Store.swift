f5556e571ca6a84702b3696f8d1edd3bb37450af
koala panda <koakoa8823@protonmail.com>
2023-12-20 13:29:13 +0900

    Storeクラスへの変数の追加

--- ToDoProject/Grobal State/Store.swift
+++ ToDoProject/Grobal State/Store.swift
@@ -21,16 +21,37 @@ struct WeatherAppApp: App {
 }*/
 
 class Store: ObservableObject {
+    let defaults = UserDefaults.standard
+    
     ///グローバルオブジェクト
-    @Published var selectedUnit: TemperatureUnit = .kelvin
-    @Published var weatherList: [WeatherViewModel] = [WeatherViewModel]()
+    @Published var indexLabel0 = "1"{
+        didSet {
+            defaults.set(indexLabel0, forKey: "label0")
+        }
+    }
+    
+    @Published var indexLabel1 = "2"{
+        didSet {
+            defaults.set(indexLabel1, forKey: "label1")
+        }
+    }
+    
+    @Published var indexLabel2 = "3"{
+        didSet {
+            defaults.set(indexLabel2, forKey: "label2")
+        }
+    }
+    
     
     init(){
-        selectedUnit = UserDefaults.standard.unit
+        //お気に入り登録されたタスクを取得
+        self.indexLabel0 = defaults.string(forKey:"label0") ?? "1"
+        self.indexLabel1 = defaults.string(forKey:"label1") ?? "2"
+        self.indexLabel2 = defaults.string(forKey:"label2") ?? "3"
     }
     
     ///都市名を配列に追加する関数
-    func addWeather(_ weather: WeatherViewModel) {
-        weatherList.append(weather)
+    func getIndexArray() -> [String]{
+        return [indexLabel0, indexLabel1, indexLabel2]
     }
 }
