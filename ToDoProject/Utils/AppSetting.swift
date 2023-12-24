b8a216bda75bdcc5bfd38651ffe512840a0f930e
koala panda <koakoa8823@protonmail.com>
2023-12-24 18:32:35 +0900

    不要なコードの削除

--- ToDoProject/Utils/AppSetting.swift
+++ ToDoProject/Utils/AppSetting.swift
@@ -32,29 +32,29 @@ class AppSetting{
 
 extension AppSetting{
     
-    ///キーボード閉じるボタンの挙動
-    static func closeKeyBoard(){
-        let keyWindow = UIApplication.shared.connectedScenes
-            .filter({$0.activationState == .foregroundActive})
-            .map({$0 as? UIWindowScene})
-            .compactMap({$0})
-            .first?.windows
-            .filter({$0.isKeyWindow}).first
-        keyWindow?.endEditing(true)
-    }
-    
-    ///端末の国設定の通貨の単位を付ける関数
-    static func currencyString(from value: Double) -> String {
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .currency
-        formatter.currencyCode = Locale.current.currency?.identifier
-        
-        // 日本円の場合の設定
-        if formatter.currencyCode == "JPY" {
-            formatter.minimumFractionDigits = 1
-            formatter.maximumFractionDigits = 3 // 小数点第三位まで表示
-        }
-        
-        return formatter.string(from: NSNumber(value: value)) ?? ""
-    }
+//    ///キーボード閉じるボタンの挙動
+//    static func closeKeyBoard(){
+//        let keyWindow = UIApplication.shared.connectedScenes
+//            .filter({$0.activationState == .foregroundActive})
+//            .map({$0 as? UIWindowScene})
+//            .compactMap({$0})
+//            .first?.windows
+//            .filter({$0.isKeyWindow}).first
+//        keyWindow?.endEditing(true)
+//    }
+//    
+//    ///端末の国設定の通貨の単位を付ける関数
+//    static func currencyString(from value: Double) -> String {
+//        let formatter = NumberFormatter()
+//        formatter.numberStyle = .currency
+//        formatter.currencyCode = Locale.current.currency?.identifier
+//        
+//        // 日本円の場合の設定
+//        if formatter.currencyCode == "JPY" {
+//            formatter.minimumFractionDigits = 1
+//            formatter.maximumFractionDigits = 3 // 小数点第三位まで表示
+//        }
+//        
+//        return formatter.string(from: NSNumber(value: value)) ?? ""
+//    }
 }
