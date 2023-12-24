93ac0456151b03fa2f393e2a12073e6a0e3aac95
koala panda <koakoa8823@protonmail.com>
2023-12-24 19:03:18 +0900

    不要なコードの削除

--- ToDoProject/ViewModels/UnitCalScreenViewModel.swift
+++ ToDoProject/ViewModels/UnitCalScreenViewModel.swift
@@ -16,6 +16,7 @@ class UnitCalScreenViewModel: ObservableObject{
     ///量を格納する変数
     @Published var amount: Double = 0
     
+    
     ///量を格納する変数
     var unit: Double{
         
@@ -29,30 +30,6 @@ class UnitCalScreenViewModel: ObservableObject{
             return num
         }
     }
-    
-    
-//    func unitCalculation(price: Double, amount: Double) -> Double{
-//        let num = price / amount
-//
-//        if num.isInfinite{
-//            return 0
-//        }else if num.isNaN{
-//            return 0
-//        }else{
-//            return num
-//        }
-//    }
-    
-//    ///キーボード閉じるボタンの挙動
-//    static func closeKeyBoard(){
-//        let keyWindow = UIApplication.shared.connectedScenes
-//            .filter({$0.activationState == .foregroundActive})
-//            .map({$0 as? UIWindowScene})
-//            .compactMap({$0})
-//            .first?.windows
-//            .filter({$0.isKeyWindow}).first
-//        keyWindow?.endEditing(true)
-//    }
     
     ///端末の国設定の通貨の単位を付ける関数
     func currencyStringFromUnit() -> String {
